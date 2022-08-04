ad0e8d1d636defa438ea1342f8fd9e721bd76be3
Sem Kholodov <kholodovsem@gmail.com>
2022-08-04 14:29:05 +0300

    Update Filter.jsx

--- src/components/ContactsBookApp,jsx/Filter.jsx
+++ src/components/ContactsBookApp,jsx/Filter.jsx
@@ -1,24 +1,18 @@
-import React, {Component} from 'react';
 import style from './Filter.module.css';
 
-class Filter extends  Component {
-
-
-
-  render() {
+function Filter ({onHandleChange,filter }) {
     return (
       <div>
         <p className={style.text}>Find contacts by name</p>
         <input
           className={style.input}
-          onChange={this.props.onHandleChange}
+          onChange={onHandleChange}
           type='text'
-          value={this.props.filter}
+          value={filter}
           name="filter"
         />
       </div>
     )
-  }
 }
 
 export default Filter;
